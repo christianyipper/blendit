ca0ba55b414523009f33bea0a98e622d4c69a71a
ssupix <151896598+ssupix@users.noreply.github.com>
2024-10-12 21:44:34 -0700

    Update Hero.js
    -Refactored code structure

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,20 +9,26 @@ const SpinningBowlSelector = ({ selectedBowl, bowls, circleColor }) => {
     // Calculate the angle of each bowl in the circle
     const bowlCount = bowls.length;
     const angle = 360 / bowlCount;
-    // Calculate the radius of the circle
     const radius = 290;
 
     return (
-        <div className="relative mx-auto w-full h-full">
+        <div className="
+            relative 
+            mx-auto 
+            w-full h-full
+        ">
             {/* Background circle */}
             <div 
-                className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-[-1] transition-colors duration-300`}
+                className="
+                    absolute 
+                    top-1/2 left-1/2 
+                    transform -translate-x-1/2 -translate-y-1/2 z-[-1] 
+                    transition-colors duration-300"
                 style={{
                     width: `${radius * 2}px`,
                     height: `${radius * 2}px`,
                 }}
             >
-                {/* Render the circle background */}
                 <svg viewBox="0 0 100 100" className="w-full h-full">
                     <circle cx="50" cy="50" r="50" className={`${circleColor} transition-colors duration-300`} />
                 </svg>
@@ -30,7 +36,9 @@ const SpinningBowlSelector = ({ selectedBowl, bowls, circleColor }) => {
             
             {/* Render the bowls */}
             <div 
-                className="w-full h-full transition-transform duration-500 ease-in-out"
+                className="
+                    w-full h-full 
+                    transition-transform duration-500 ease-in-out"
                 style={{ transform: `rotate(${-selectedBowl * angle}deg)` }}
             >
                 {bowls.map((bowl, index) => {
@@ -43,7 +51,10 @@ const SpinningBowlSelector = ({ selectedBowl, bowls, circleColor }) => {
                     return (
                         <div
                             key={index}
-                            className="absolute top-1/2 left-1/2 size-52 transition-all duration-300"
+                            className="
+                                absolute top-1/2 left-1/2 
+                                size-52 
+                                transition-all duration-300"
                             style={{
                                 // Move the bowl to the correct position in the circle
                                 transform: `translate(-50%, -50%) translate(${x}px, ${y}px) rotate(${selectedBowl * angle}deg)`,
@@ -53,7 +64,13 @@ const SpinningBowlSelector = ({ selectedBowl, bowls, circleColor }) => {
                             <img 
                                 src={bowl} 
                                 alt={`Smoothie Bowl ${index + 1}`} 
-                                className={`w-full h-full object-cover rounded-full transition-all duration-300 ${index === selectedBowl ? 'scale-125 shadow-lg' : 'scale-100'}`}
+                                className={`
+                                    w-full h-full 
+                                    object-cover 
+                                    rounded-full 
+                                    transition-all duration-300 
+                                    ${index === selectedBowl ? 'scale-125 shadow-lg' : 'scale-100'}
+                                `}
                             />
                         </div>
                     );
@@ -84,46 +101,80 @@ function Hero() {
     }, [selectedBowl]);
 
     return (
-        <section className="w-full h-screen grid relative overflow-hidden p-5 bg-background mobile:grid-cols-4">
-            <div className="col-span-4 flex flex-col gap-5 mt-10">
+        <section className="
+            w-full h-screen grid
+            grid-cols-4 p-5 gap-20
+            relative 
+            overflow-hidden
+            bg-background
+        ">
+            <svg viewBox="0 0 100 100" className="w-full h-full absolute">
+                    <circle cx={'120%'} cy={'0%'} r={'100%'} className={`${colorScheme.secondary} transition-colors duration-300`} />
+            </svg>
+
+            <div className="
+                col-span-4 
+                flex flex-col gap-5 
+                mt-10
+                z-[2]
+            ">
                 <h3 className={`transition-colors duration-300 ${colorScheme.text}`}>
-                    {/* Title is pink by default, and changes color based on the selected bowl */}
                     Top it. Love it.
                 </h3>
                 <img
-                    className="w-full max-w-md transition-filter duration-300"
-                    alt="Blend It."
-                    src={heroLogo}
-                    style={{ filter: `hue-rotate(${selectedBowl * 90}deg)` }}
+                    className="
+                        w-full max-w-md 
+                        transition-filter duration-300"
+                        src={heroLogo}
+                        alt="Blend It."
+                        style={{ filter: `hue-rotate(${selectedBowl * 90}deg)` }}
                 />
-                <p>
-                    {/* Text is pink by default, and changes color based on the selected bowl */}
+                <p className='text-grey-900'>
                     Welcome to Blend It, where healthy eating is effortless and fun! Order fresh nutritious smoothie bowls with uniquely interactive and customizable ordering experience. We put the power of choice in your own hands!
                 </p>
-                <div className='flex gap-5 justify-center'>
-                    {/* Buttons are pink by default, and change color based on the selected bowl */}
-                    <button className={`w-fit text-white py-3 px-6 rounded-full text-nowrap transition-colors duration-300 ${colorScheme.primary}`}>
+                <div className="flex gap-5 justify-center">
+                    <button className={`
+                        w-fit 
+                        py-3 px-6 
+                        text-white 
+                        rounded-full 
+                        text-nowrap 
+                        ${colorScheme.primary}
+                        transition-colors duration-300
+                    `}>
                         Customize
                     </button>
-                    <button className={`w-fit py-3 px-6 rounded-full text-nowrap transition-colors duration-300 border-2 ${colorScheme.text.replace('text', 'border')} ${colorScheme.text}`}>
+                    <button className={`
+                        w-fit 
+                        py-3 px-6 
+                        rounded-full 
+                        text-nowrap 
+                        ${colorScheme.text}
+                        ${colorScheme.text.replace('text', 'border')} 
+                        transition-colors duration-300 border-2 
+                    `}>
                         View Menu
                     </button>
                 </div>
                 <div className='flex gap-3 justify-center mt-6'>
-                    {/* Bowl images are pink by default, and change color based on the selected bowl */}
                     {bowls.map((bowl, index) => (
                         <img
                             key={index}
                             src={bowl}
                             alt={`Smoothie Bowl ${index + 1}`}
-                            className='size-20 cursor-pointer rounded-full'
+                            className="size-20 cursor-pointer rounded-full shadow-md"
                             onClick={() => setSelectedBowl(index)}
                         />
                     ))}
                 </div>
             </div>
 
-            <section className='col-span-4 translate-y-40 flex align-end mt-10 min-h-full'>
+            <section className="
+                col-span-4 
+                flex align-end 
+                mt-10
+                translate-y-40 
+            ">
                 <SpinningBowlSelector selectedBowl={selectedBowl} bowls={bowls} circleColor={colorScheme.secondary} />
             </section>
         </section>
