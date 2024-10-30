56d81fea92747c71d4176f7d4401ee90b7cfcb82
Christian <christianyippers@gmail.com>
2024-10-29 18:45:20 -0700

    Navigation

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import BlenditSVG from '../assets/icons/BlenditSVG';
+import Button from './Button';
 
 function Nav() {
 
@@ -8,8 +10,37 @@ function Nav() {
     };
 
     return (
-        <section className="w-full">
-
+        <section className="
+        w-full p-4 top-0 mt-0
+        fixed z-50
+        bg-grey-100
+        ">
+            <div className="
+            flex flex-row justify-between items-center
+            ">
+                <BlenditSVG
+                    color="#D31A74"
+                />
+                <div className="
+                flex flex-row justify-between items-center
+                gap-4
+                ">
+                    <Button
+                        text="Order ahead"
+                        color="text-white"
+                        bg="bg-pink-500"
+                        border="border-pink-500"
+                    />
+                    <div className="
+                    w-12 h-10 
+                    relative flex flex-col items-center justify-between
+                    ">
+                        <div className="w-full h-1 rounded-lg bg-pink-500"/>
+                        <div className="w-full h-1 rounded-lg bg-pink-500"/>
+                        <div className="w-full h-1 rounded-lg bg-pink-500"/>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 }
